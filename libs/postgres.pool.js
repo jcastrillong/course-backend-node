4666545f9926221b251b7cb82eae070ceaf05e55
jcastrillong <jhonatancastrillon34@gmail.com>
2022-04-02 13:33:16 -0500

    variables de entorno

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,11 +1,13 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
+
+const { config } = require("../config/config");
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const pool = new Pool({
-  user: 'jcastrillong',
-  host: 'localhost',
-  database: 'jcastrillong',
-  password: 'password123',
-  port: 5432,
+  connectionString: URI,
 });
 
 module.exports = pool;
