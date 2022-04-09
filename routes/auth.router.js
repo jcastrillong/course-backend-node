c93659c910b20e8c9a8587350f050bbba67e258f
jcastrillong <jhonatancastrillon34@gmail.com>
2022-04-08 23:23:38 -0500

    implementando JWT

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,16 +1,30 @@
 const { Router } = require("express");
 const passport = require("passport");
+const jwt = require("jsonwebtoken");
+
+const { config } = require("./../config/config");
 
 const router = Router();
 
-router.post("/login", 
-  passport.authenticate("local", {session: false}),
+router.post(
+  "/login",
+  passport.authenticate("local", { session: false }),
   async (req, res, next) => {
     try {
-      res.json(req.user);
+      const user = req.user;
+      const payload = {
+        sub: user.id,
+        role: user.role,
+      };
+      const token = jwt.sign(payload, config.jwtSecret);
+      res.json({
+        user,
+        token,
+      });
     } catch (error) {
       next(error);
     }
-});
+  }
+);
 
 module.exports = router;
